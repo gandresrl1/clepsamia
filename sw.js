0fb0053e5a04f81875b17780bda6ff2b33518f84
gandresrl1 <gandresrl@msn.com>
2026-05-12 09:19:51 -0500

    init
    app clepsamia - reloj de arena

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'temporizador-v1';
+const FILES = ['./index.html', './manifest.json', './icon.svg', './freesound_community-alarm-26718.mp3'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
